6a1c4a709b144d7bb5fdfde5b58bf51c51dedc29
Hai Thanh Dinh <anhchangtoanhoc97@gmail.com>
2023-01-08 21:57:06 +0700

    Finish CreditCard part

--- src/components/sections/CreditCard.jsx
+++ src/components/sections/CreditCard.jsx
@@ -1,23 +1,37 @@
 import React from "react";
 import CreditCardImage from "../../images/illustrations/credit-card.png";
+import SecondaryButton from "../buttons/SecondaryButton";
+import { BsCheckCircleFill } from "react-icons/bs";
 function CreditCard(props) {
   return (
     <div className="container mx-auto my-10 justify-center">
-      <div className="grid grid-cols-2">
-        <div>
+      <div className="grid grid-cols-2 gap-10 items-center my-36">
+        <div className="ml-14">
           <img src={CreditCardImage} alt="" />
         </div>
-        <div>
-          <h2>
+        <div className="ml-14">
+          <h2 className="font-semibold text-3xl leading-normal">
             Introducing the NEFA <br /> Credit Card
           </h2>
-          <p>
-            Subject to cardholder and rewards terms which will be available at
-            application.
+          <p className="font-normal mt-5">
+            Subject to cardholder and rewards terms which will be available{" "}
+            <br /> at application.
           </p>
-          <div className="border border-primary rounded-full">
-            <p>Join the waitlist</p>
-          </div>
+          <ul className="py-2 mt-5">
+            <li className="py-2">
+              <BsCheckCircleFill color={"blue"} className="inline mx-5" />
+              Up to 3% back on purchases
+            </li>
+            <li className="py-2">
+              <BsCheckCircleFill color={"blue"} className="inline mx-5" />
+              Earn rewards in bitcoin or any crypto on NEFA
+            </li>
+            <li className="py-2">
+              <BsCheckCircleFill color={"blue"} className="inline mx-5" />
+              No annual fee
+            </li>
+          </ul>
+          <SecondaryButton>Join the waitlist</SecondaryButton>
         </div>
       </div>
     </div>
